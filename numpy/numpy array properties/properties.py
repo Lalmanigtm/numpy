02799c178aa84cfff37643170e78558d3c3932b5
Saurab Gautam <missionprojects250@gmail.com>
2026-07-29 15:11:18 +0545

    04-properties in numpy

---
+++ numpy/numpy array properties/properties.py
@@ -0,0 +1,30 @@
+# shape
+# import numpy as np
+
+# arr_2d = np.array([[1,2,3],
+#                   [4,5,2]])
+# print(arr_2d.shape)   
+
+# size = total number of element
+# import numpy as np
+
+# array = np.array([[1,2,3],[4,5,6]])
+# print(array.size)
+
+# ndim = number of dimension
+# import numpy as np
+
+# arr_1d = np.array([1,2])  # 1 dimensional array
+# arr_2d = np.array([[1,2],[3,4]])  # 2 dimensional array
+# arr_3d = np.array([[[1,2],[3,4],[5,6],[7,8]]])  # 3 dimensional array
+
+# print(arr_3d.ndim)
+# print(arr_1d.ndim)
+# print(arr_2d.ndim)
+
+# dtype = represent the datatype of a elements like string, integer, float,....all
+
+import numpy as np
+
+arr_1d = np.array([1.0,2])
+print(arr_1d.dtype) 
